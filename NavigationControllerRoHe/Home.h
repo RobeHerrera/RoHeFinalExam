34d20a9a7c5c5920877a5dc12aac8e2492d94175
RobertoHerrera <robertoherrera@robertoherreras-mac.local>
2016-06-27 12:01:12 -0500

    Initial Configuration

---
+++ NavigationControllerRoHe/Home.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationControllerRoHe
+//
+//  Created by RobertoHerrera on 6/27/16.
+//  Copyright © 2016 RobertoHerrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Home : UIViewController
+
+
+@end
+
---
+++ NavigationControllerRoHe/Home.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NavigationControllerRoHe
+//
+//  Created by RobertoHerrera on 6/27/16.
+//  Copyright © 2016 RobertoHerrera. All rights reserved.
+//
+
+#import "Home.h"
+
+@interface Home ()
+
+@end
+
+@implementation Home
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
